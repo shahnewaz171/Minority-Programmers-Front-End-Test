89d40300836aaee31c4a93ef9af829b0cda5c05f
shahnewaz601 <shahnewaz601@gmail.com>
2021-06-17 02:47:17 +0600

    navbar done

--- src/components/shared/Navbar/Navbar.js
+++ src/components/shared/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link, NavLink } from 'react-router-dom';
 import Logo from "../../../images/logo/website-logo.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter, faFacebookSquare, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
@@ -9,7 +9,7 @@ import './Navbar.css';
 const Navbar = () => {
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light bg-white">
                 <div className="container">
                     <Link to="#" className="navbar-brand nav-logo">
                         <img src={Logo} className="img-fluid" alt="logo" />
@@ -20,41 +20,39 @@ const Navbar = () => {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link to="#" className="nav-link ps-lg-5 twitter-icon">
+                                <NavLink to="#"  className="nav-link ps-lg-5 twitter-icon">
                                     <FontAwesomeIcon icon={faTwitter} className="nav-icons" />
-                                </Link>
+                                </NavLink>
                             </li>
                             <li className="nav-item ps-2">
-                                <Link to="#" className="nav-link">
+                                <NavLink to="#" className="nav-link">
                                     <FontAwesomeIcon icon={faFacebookSquare} className="nav-icons" />
-                                </Link>
+                                </NavLink>
                             </li>
                             <li className="nav-item ps-2">
-                                <Link to="#" className="nav-link">
+                                <NavLink to="#" className="nav-link">
                                     <FontAwesomeIcon icon={faLinkedin} className="nav-icons" />
-                                </Link>
+                                </NavLink>
                             </li>
                             <li className="nav-item ps-2">
-                                <Link to="#" className="nav-link">
+                                <NavLink to="#" className="nav-link">
                                     <FontAwesomeIcon icon={faInstagram} className="nav-icons" />
-                                </Link>
+                                </NavLink>
                             </li>
                         </ul>
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li className="nav-item pe-2">
-                                <Link to="#" className="nav-link fw-bold">Home</Link>
+                            <li className="nav-item pe-3">
+                                <NavLink to="/home" exact activeClassName="nav-link-active" className="nav-link fw-bold">Home</NavLink>
                             </li>
-                            <li className="nav-item pe-2">
-                                <Link to="#" className="nav-link fw-bold">Incubator</Link>
+                            <li className="nav-item pe-3">
+                                <NavLink to="/incubator" exact activeClassName="nav-link-active" className="nav-link fw-bold">Incubator</NavLink>
                             </li>
-                            <li className="nav-item pe-2">
-                                <Link to="#" className="nav-link fw-bold">Learn</Link>
+                            <li className="nav-item pe-3">
+                                <NavLink to="/learn" exact activeClassName="nav-link-active" className="nav-link fw-bold">Learn</NavLink>
                             </li>
-                            <li className="nav-item search-btn d-flex justify-content-center align-items-center">
-                                <FontAwesomeIcon icon={faSearch} />
-                                <div className="togglesearch">
-                                    <input type="text" placeholder=""/>
-                                    <input type="button" value="Search"/>
+                            <li className="nav-item">
+                                <div className="search-btn d-flex justify-content-center align-items-center">
+                                    <FontAwesomeIcon icon={faSearch} />
                                 </div>
                             </li>
                         </ul>
