5d9c2b4ae0228ce25ebb420b5418333915a05797
shahnewaz601 <shahnewaz601@gmail.com>
2021-06-17 01:25:40 +0600

    install font-awesome and update navbar

--- src/components/shared/Navbar/Navbar.js
+++ src/components/shared/Navbar/Navbar.js
@@ -1,23 +1,53 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import Logo from "../../../images/logo/website-logo.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faFacebookSquare, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import './Navbar.css';
 
 const Navbar = () => {
     return (
         <>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <Link to="#" class="navbar-brand">Navbar</Link>
+            <nav class="navbar navbar-expand-lg navbar-light bg-white">
+                <div class="container">
+                    <Link to="#" class="navbar-brand nav-logo">
+                        <img src={Logo} className="img-fluid" alt="logo" />
+                    </Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
+                                <Link to="#" class="nav-link">
+                                    <FontAwesomeIcon icon={faTwitter} />
+                                </Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link to="#" class="nav-link">
+                                    <FontAwesomeIcon icon={faFacebookSquare} />
+                                </Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link to="#" class="nav-link">
+                                    <FontAwesomeIcon icon={faLinkedin} />
+                                </Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link to="#" class="nav-link">
+                                    <FontAwesomeIcon icon={faInstagram} />
+                                </Link>
+                            </li>
+                        </ul>
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
                                 <Link to="#" class="nav-link">Home</Link>
                             </li>
                             <li class="nav-item">
-                                <Link to="#" class="nav-link">About</Link>
+                                <Link to="#" class="nav-link">Incubator</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link to="#" class="nav-link">Learns</Link>
                             </li>
                         </ul>
                     </div>
