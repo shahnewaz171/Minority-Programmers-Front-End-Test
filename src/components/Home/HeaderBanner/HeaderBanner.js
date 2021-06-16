86c0e9f69515a1ec1865dad153c21407eebe9375
shahnewaz601 <shahnewaz601@gmail.com>
2021-06-17 03:27:36 +0600

    header banner done

--- src/components/Home/HeaderBanner/HeaderBanner.js
+++ src/components/Home/HeaderBanner/HeaderBanner.js
@@ -6,8 +6,11 @@ const HeaderBanner = () => {
         <div className="mb-4 headerBanner">
             <div className="h-100 d-flex align-items-center">
                 <div className="col-md-6"></div>
-                <div className="col-12 col-md-5 d-flex align-items-center justify-content-center">
+                <div className="col-12 col-md-5 d-flex flex-column align-items-center justify-content-center">
                     <h1 className="text-white">Invest directly into <br /> minority innovations.</h1>
+                    <button className="btn px-4 banner-btn mt-3">
+                        View Startups
+                    </button>
                 </div>
                 <div className="col-md-1"></div>
             </div>
