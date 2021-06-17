b4ebb1c5b7131649823bf81a29bdad7fa88a4a33
shahnewaz601 <shahnewaz601@gmail.com>
2021-06-18 03:13:13 +0600

    update startup header style

--- src/components/FundStartup/FundStartupHeader/FundStartupHeader.js
+++ src/components/FundStartup/FundStartupHeader/FundStartupHeader.js
@@ -7,13 +7,13 @@ import './FundStartupHeader.css';
 const FundStartupHeader = () => {
     return (
         <div className="mt-4 startup-header bg-white">
-            <div className="d-flex align-items-center">
-                <div className="col-md-1 d-flex justify-content-center align-items-center">
+            <div className="row align-items-center">
+                <div className="col-md-1 d-md-flex justify-content-center align-items-center startupHeader-item">
                     <div className="arrow-icon">
                         <FontAwesomeIcon icon={faArrowLeft} />
                     </div>
                 </div>
-                <div className="col-md-2">
+                <div className="col-md-2 startupHeader-item">
                     <div className="d-flex justify-content-between align-items-center">
                         <div className="mangoswap-logo">
                             <img src={logo} className="img-fluid" alt="mangoswapLogo" style={{height: "75%"}} />
@@ -22,20 +22,20 @@ const FundStartupHeader = () => {
                     </div>
                 </div>
                 <div className="col-md-1"></div>
-                <div className="col-md-3">
+                <div className="col-md-3 startupHeader-item">
                     <div className="d-flex">
                         <h6 className="fund-status">Funds raised</h6>
                         <span className="ms-4">50% complete</span>
                     </div>
                     <h6 className="fw-bold">$200,000/$400,000</h6>
                 </div>
-                <div className="col-md-3">
-                    <div className="progress ms-5">
+                <div className="col-md-3 startupHeader-item">
+                    <div className="progress ms-md-5">
                         <div className="progress-bar w-50" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
                 </div>
-                <div className="col-md-2">
-                    <button className="btn px-4 banner-btn ms-4">
+                <div className="col-md-2 startupHeader-item">
+                    <button className="btn banner-btn ms-md-4">
                         Fund Startup
                     </button>
                 </div>
