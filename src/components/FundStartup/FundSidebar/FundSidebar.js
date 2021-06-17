8ae1a3abaec813b771060be4e04c004a118f0a15
shahnewaz601 <shahnewaz601@gmail.com>
2021-06-18 04:20:00 +0600

    startup sidebar done

--- src/components/FundStartup/FundSidebar/FundSidebar.js
+++ src/components/FundStartup/FundSidebar/FundSidebar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { faFacebookF, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import './FundSidebar.css';
 
@@ -39,6 +40,52 @@ const FundSidebar = () => {
             <hr />
             <div className="px-4">
                 <h4>Meet the team</h4>
+                <div className="pt-3 d-flex">
+                    <div className="user-avater">
+                        <FontAwesomeIcon icon={faUser} />
+                    </div>
+                    <div className="ms-3">
+                        <h6 className="fw-bold">Edmund Kitan</h6>
+                        <p>Founder</p>
+                    </div>
+                </div>
+                <div className="pt-2 d-flex">
+                    <div className="user-avater">
+                        <FontAwesomeIcon icon={faUser} />
+                    </div>
+                    <div className="ms-3">
+                        <h6 className="fw-bold">Olarenwaju Cesar</h6>
+                        <p>Co-Founder</p>
+                    </div>
+                </div>
+                <div className="pt-2 d-flex">
+                    <div className="user-avater">
+                        <FontAwesomeIcon icon={faUser} />
+                    </div>
+                    <div className="ms-3">
+                        <h6 className="fw-bold">Uzo Amanda</h6>
+                        <p>Head of Growth</p>
+                    </div>
+                </div>
+                <div className="pt-2 d-flex">
+                    <div className="user-avater">
+                        <FontAwesomeIcon icon={faUser} />
+                    </div>
+                    <div className="ms-3">
+                        <h6 className="fw-bold">Donald Duke</h6>
+                        <p>Head of Product</p>
+                    </div>
+                </div>
+            </div>
+            <hr />
+            <div className="px-4 sidebar-tags pb-3">
+                <h4>Tags</h4>
+                <div className="mt-3">
+                    <button className="btn btn-outline-secondary px-2 py-1">Crypto</button>
+                    <button className="btn btn-outline-secondary ms-2 px-2 py-1">Exchanges</button>
+                    <button className="btn btn-outline-secondary ms-2 px-2 py-1">NFT</button>
+                    <button className="btn btn-outline-secondary ms-2 px-2 py-1">Blockchain</button>
+                </div>
             </div>
         </div>
     );
