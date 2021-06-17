9bc4fef144a60af20a33866bc8457b9d2771707c
shahnewaz601 <shahnewaz601@gmail.com>
2021-06-18 03:53:56 +0600

    update startup sidebar section

--- src/components/FundStartup/FundSidebar/FundSidebar.js
+++ src/components/FundStartup/FundSidebar/FundSidebar.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import './FundSidebar.css';
 
 const FundSidebar = () => {
     return (
-        <div>
-            
+        <div className="bg-white fundSidebar-style">
+            <h6 className="fw-bold pt-3 pb-1 px-4">Founded 17th May, 2015</h6>
+            <hr />
+            <div className="px-4">
+                <div className="pb-3">
+                    <p className="mb-1">Website</p>
+                    <div className="d-flex">
+                        <a href="https://minorityprogrammers.com" className="text-decoration-none fw-bold">mangoswap.com</a>
+                        <div className="ms-3">
+                            <a href="https://twitter.com/minorityprogram">
+                                <FontAwesomeIcon icon={faTwitter} />
+                            </a>
+                            <a href="https://www.linkedin.com/company/minority-programmers/" className="ms-2">
+                                <FontAwesomeIcon icon={faLinkedinIn} />
+                            </a>
+                            <a href="https://www.facebook.com/MinorityProgrammers" className="ms-2">
+                                <FontAwesomeIcon icon={faFacebookF} />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="pb-3">
+                    <p className="mb-1">Location</p>
+                    <h5>San Francisco, California</h5>
+                    
+                </div>
+                <div>
+                    <p className="mb-1">Team Size</p>
+                    <h5>100-500 people</h5>
+                </div>
+            </div>
+            <hr />
+            <div className="px-4">
+                <h4>Meet the team</h4>
+            </div>
         </div>
     );
 };
--- src/components/FundStartup/FundStartup.js
+++ src/components/FundStartup/FundStartup.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import FundSidebar from './FundSidebar/FundSidebar';
 import FundStartupHeader from './FundStartupHeader/FundStartupHeader';
 
 const FundStartup = () => {
     return (
         <div className="container">
             <FundStartupHeader />
+            <div className="row mt-3">
+                <div className="col-md-4">
+                    <FundSidebar />
+                </div>
+            </div>
         </div>
     );
 };
