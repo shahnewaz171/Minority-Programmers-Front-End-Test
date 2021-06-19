c501fe1e2e10664f0df3e3841247ae42af7796bc
shahnewaz601 <shahnewaz601@gmail.com>
2021-06-20 04:39:16 +0600

    dynamic testimonials component

--- src/components/Home/Testimonials/Testimonials.js
+++ src/components/Home/Testimonials/Testimonials.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import memberLogo from '../../../images/home/members-logo.png';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import testimonialsInfo from '../../../dummyData/testimonialsData.json';
 import './Testimonials.css';
 import settings from '../../IncubatorPage/FeaturedStartups/settings';
 
 const Testimonials = () => {
+    const [testimonials, setTestimonials] = useState([]);
+
+    useEffect(() => {
+        setTestimonials(testimonialsInfo);
+    }, [])
+
     return (
         <div className="testimonials">
             <div className="container">
@@ -16,61 +23,19 @@ const Testimonials = () => {
                 </div>
                 <div className="row mt-5">
                     <Slider {...settings}>
-                        <div className="col-md-4">
-                            <div className="mb-md-5 pb-4">
-                                <div className="courses-items px-3 mb-4 mx-2">
-                                    <div className="testimonials-img mb-4 pt-4">
-                                        <img src={memberLogo} className="img-fluid" alt="" />
+                        {testimonials && testimonials.map(testimonial => (
+                            <div key={testimonial.id} className="col-md-4">
+                                <div className="mb-md-5 pb-4">
+                                    <div className="courses-items px-3 mb-4 mx-2">
+                                        <div className="testimonials-img mb-4 pt-4">
+                                            <img src={memberLogo} className="img-fluid" alt="" />
+                                        </div>
+                                        <h4>{testimonial.name}</h4>
+                                        <p className="pb-2 text-center">{testimonial.designation}</p>
                                     </div>
-                                    <h4>Henry Gomez</h4>
-                                    <p className="pb-2 text-center">CEO, MasterTech</p>
                                 </div>
                             </div>
-                        </div>
-                        <div className="col-md-4">
-                            <div className="mb-md-5 pb-4">
-                                <div className="courses-items px-3 mb-4 mx-2">
-                                    <div className="testimonials-img mb-4 pt-4">
-                                        <img src={memberLogo} className="img-fluid" alt="" />
-                                    </div>
-                                    <h4>Henry Gomez</h4>
-                                    <p className="pb-2 text-center">CEO, MasterTech</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4">
-                            <div className="mb-md-5 pb-4">
-                                <div className="courses-items px-3 mb-4 mx-2">
-                                    <div className="testimonials-img mb-4 pt-4">
-                                        <img src={memberLogo} className="img-fluid" alt="" />
-                                    </div>
-                                    <h4>Henry Gomez</h4>
-                                    <p className="pb-2 text-center">CEO, MasterTech</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4">
-                            <div className="mb-md-5 pb-4">
-                                <div className="courses-items px-3 mb-4 mx-2">
-                                    <div className="testimonials-img mb-4 pt-4">
-                                        <img src={memberLogo} className="img-fluid" alt="" />
-                                    </div>
-                                    <h4>Henry Gomez</h4>
-                                    <p className="pb-2 text-center">CEO, MasterTech</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4">
-                            <div className="mb-md-5 pb-4">
-                                <div className="courses-items px-3 mb-4 mx-2">
-                                    <div className="testimonials-img mb-4 pt-4">
-                                        <img src={memberLogo} className="img-fluid" alt="" />
-                                    </div>
-                                    <h4>Henry Gomez</h4>
-                                    <p className="pb-2 text-center">CEO, MasterTech</p>
-                                </div>
-                            </div>
-                        </div>
+                        ))}
                     </Slider>
                 </div>
             </div>
